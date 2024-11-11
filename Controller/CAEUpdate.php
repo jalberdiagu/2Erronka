13b151204da00edd70e5c1c29a9be3b329e536ed
2AW3 User 21 <2AW3-21@yourdomain.com>
2024-11-11 11:14:53 +0100

    11/11/2024

--- Controller/CAEUpdate.php
+++ Controller/CAEUpdate.php
@@ -6,7 +6,7 @@ $data = file_get_contents("php://input");
 require_once("/../Model/Konexioa.php");
 
 $query = $pdo->prepare("SELECT * FROM erabiltzaileak WHERE id_erab = :id");
-
+//sigue copiando reinaaa
 $query-> bindParam(":id", $data);
 $query->execute();
 $erantzuna = $query->fetch(PDO::FETCH_ASSOC);
