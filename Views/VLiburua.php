4687adde945ea4f94cc59736eb6107a1391063f5
2AW3 User 21 <2AW3-21@yourdomain.com>
2024-11-13 11:29:59 +0100

    13/11/20024 git commit -m 13/11/20024

--- Views/VLiburua.php
+++ Views/VLiburua.php
@@ -7,22 +7,23 @@ class VLiburua extends Vista
 
     public function Libro($libro)
     {
-            ?>
-            <article id="producto">
+        ?>
+        <div id="producto">
             <div id="portada">
                 <img src="<?= $libro["argazkia"]; ?> ">
             </div>
             <div id="texto">
                 <h1><?= $libro["izenburua"]; ?></h1>
                 <h2><a href='CLiburuak.php?writer=<?=
-                                $libro["idazlea"];
-                                    ?>'>><?= $libro["idazlea"]; ?></a></h2>
+                    $libro["idazlea"];
+                ?>'><?= $libro["idazlea"]; ?></a></h2>
+                 <h3><?= $libro["prezioa"]; ?>€</h3>
                 <p><?= $libro["argitaletxea"]; ?> - <?= $libro["isbn"]; ?></p>
                 <p><?= $libro["deskripzioa"]; ?></p>
+                <button class="add-to-cart">Gehitu Orgara</button>
             </div>
-            </article>
-
+        </div>
     <?php }
 
 
-}?>
+} ?>
--- Views/VLiburuak.php
+++ Views/VLiburuak.php
@@ -4,34 +4,67 @@ require_once __DIR__ . '/Vista.php';
 
 class VLiburuak extends Vista
 {
-
     public function Liburuak($libros)
     {
+        $generoSeleccionado = isset($_GET['generoa']) ? $_GET['generoa'] : '';
+
+        if ($generoSeleccionado) {
+            $libros = array_filter($libros, function ($libro) use ($generoSeleccionado) {
+                return $libro["generoa"] === $generoSeleccionado;
+            });
+        }
+
+        $generos = array_unique(array_column($libros, 'generoa'));
+
         ?>
-        <?php
-        foreach ($libros as $libro) {
-            ?>
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $libro["izenburua"]; ?></h5>
-                    <p class="card-text"><?= $libro["idazlea"]; ?></p>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><?= $libro["prezioa"]; ?></li>
-                    <li class="list-group-item"><?= $libro["generoa"]; ?></li>
-                </ul>
-                <div class="card-body">
-                    <a href="CLiburua.php?izenburua=<?php
-                    $libro["izenburua"];
-                    ?>" class="card-link"><button type="button" class="btn btn-primary">Ikusi
-                            Liburua</button></a>
 
-                </div>
+        <!-- Formulario de filtro -->
+        <form method="GET" action="">
+            <div class="mb-3">
+                <label for="generoa" class="form-label">Generoa Aukeratu</label>
+                <select name="generoa" id="generoa" class="form-select">
+                    <option value="">Genero guztiak</option>
+                    <?php foreach ($generos as $genero) { ?>
+                        <option value="<?= $genero; ?>" <?= $genero === $generoSeleccionado ? 'selected' : ''; ?>>
+                            <?= $genero; ?>
+                        </option>
+                    <?php } ?>
+                </select>
+                <button type="submit" class="btn btn-primary mt-2">Filtratu</button>
             </div>
-        <?php }
-    ?>
-    <?php }
+        </form>
 
-
+        <div class="row">
+            <?php
+            if (empty($libros)) {
+                echo "<p>Ez dago libururik genero honekin</p>";
+            } else {
+                foreach ($libros as $libro) {
+                    ?>
+                    <div class="col-sm-3 mb-3 mb-sm-0">
+                        <div class="card" style="width: 21rem;">
+                            <img src="<?= $libro["argazkia"]; ?>" class="card-img-top" alt="<?= $libro["izenburua"]; ?>">
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $libro["izenburua"]; ?></h5>
+                                <p class="card-text"><?= $libro["idazlea"]; ?></p>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item"><?= $libro["prezioa"]; ?></li>
+                                <li class="list-group-item"><?= $libro["generoa"]; ?></li>
+                            </ul>
+                            <div class="card-body">
+                                <a href="CLiburua.php?book=<?= urlencode($libro["izenburua"]); ?>" class="card-link">
+                                    <button type="button" class="btn btn-primary">Ikusi Liburua</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+                }
+            }
+            ?>
+        </div>
+        
+        <?php
+    }
 }
